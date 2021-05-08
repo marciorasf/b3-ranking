2e074b2b0bf3680b71f8c3f76e551dce86266d0b
marciorasf <marciorasf@gmail.com>
2021-05-08 12:53:10 -0300

    add function to filter stocks

---
+++ src/services/filter-stocks.ts
@@ -0,0 +1,37 @@
+import Stock from "../types/stock";
+import { Indicator } from "../types/stock-indicators";
+
+type FilterOptions = {
+  min?: number;
+  max?: number;
+};
+
+type Filters = Partial<Record<Indicator, FilterOptions>>;
+
+export default function filterStocks(stocks: Stock[], filters: Filters) {
+  let filteredStocks = stocks;
+  Object.entries(filters).forEach((entry) => {
+    const indicator = entry[0] as Indicator;
+    const filter = entry[1] as FilterOptions;
+
+    filteredStocks = filteredStocks.filter((stock) => {
+      const indicatorValue = stock.indicatorsValues[indicator];
+
+      if (!indicatorValue) {
+        return false;
+      }
+
+      if (filter.min !== undefined && indicatorValue < filter.min) {
+        return false;
+      }
+
+      if (filter.max !== undefined && indicatorValue > filter.max) {
+        return false;
+      }
+
+      return true;
+    });
+  });
+
+  return filteredStocks;
+}
