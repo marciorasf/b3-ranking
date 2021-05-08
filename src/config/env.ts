c7a25158f5474f9e4766347ca40816c0d78e81d1
marciorasf <marciorasf@gmail.com>
2021-05-08 16:00:57 -0300

    add .env var

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,4 +6,4 @@ export const __dev__ = process.env.NODE_ENV === "development";
 
 export const __port__ = process.env.PORT || 3333;
 
-export const __import_fold_length__ = 20;
+export const __import_fold_length__ = process.env.IMPORT_FOLD_LENGTH;
