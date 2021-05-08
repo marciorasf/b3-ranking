c0a0074d98c543dab29fcd19f2b24dfe7504b6c9
marciorasf <marciorasf@gmail.com>
2021-05-08 16:00:03 -0300

    clean un code

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,5 +7,3 @@ export const __dev__ = process.env.NODE_ENV === "development";
 export const __port__ = process.env.PORT || 3333;
 
 export const __import_fold_length__ = 20;
-
-export const __use_dev_stocks__ = false;
--- src/functions/get-stocks-codes.ts
+++ src/functions/get-stocks-codes.ts
@@ -1,11 +1,7 @@
 import fs from "fs";
 
-import { __use_dev_stocks__ } from "../config/env";
-
 export default async function getStocksCodes(): Promise<string[]> {
-  const file = __use_dev_stocks__
-    ? "resources/stocks_codes_dev.json"
-    : "resources/stocks_codes.json";
+  const file = "resources/stocks_codes.json";
 
   const rawFile = ((await fs.promises.readFile(file)) as unknown) as string;
   const availableStocks = JSON.parse(rawFile).stocks;
