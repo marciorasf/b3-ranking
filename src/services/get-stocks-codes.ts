9c9f85a24e446d7a1b61ffae7d093b8d100fb792
marciorasf <marciorasf@gmail.com>
2021-05-08 10:48:32 -0300

    use dev stocks_codes

--- src/services/get-stocks-codes.ts
+++ src/services/get-stocks-codes.ts
@@ -1,9 +1,9 @@
 import fs from "fs";
 
-import { __dev__ } from "../config/env";
+import { __use_dev_stocks__ } from "../config/env";
 
 export default async function getStocksCodes(): Promise<string[]> {
-  const file = __dev__
+  const file = __use_dev_stocks__
     ? "resources/stocks_codes_dev.json"
     : "resources/stocks_codes.json";
 
