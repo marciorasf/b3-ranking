1bc2c0f581184d7a8b1ab1ea3b37fb2cc353538a
marciorasf <marciorasf@gmail.com>
2021-05-08 12:22:19 -0300

    cleanup repo

--- src/cli.ts
+++ src/cli.ts
@@ -5,11 +5,11 @@ import Table from "cli-table";
 import program from "commander";
 
 import packageJson from "../package.json";
-import getStocksWithDefaultRankingOptions from "./services/get-stocks-with-default-ranking-options";
+import getLastImport from "./services/get-last-import";
 import importStocks from "./services/import-stocks";
 import StockWithRankingAndScore from "./types/stock-with-ranking-and-score";
 
-function showStocksTable(stocks: StockWithRankingAndScore[]) {
+export function showStocksTable(stocks: StockWithRankingAndScore[]) {
   const table = new Table({
     head: ["code", "position", "score"],
     colWidths: [10, 10, 10],
@@ -33,53 +33,26 @@ program
   });
 
 program
-  .command("default-list")
+  .command("list")
   .description("Get stocks with default ranking options")
   .option("-n [n]", "Number of stocks listed")
   .option("-f [f]", "Filter stocks of the same enterprise")
   .action(async ({ n, f: filter }) => {
-    let stocks = await getStocksWithDefaultRankingOptions(filter);
+    const lastImport = await getLastImport();
 
-    if (!stocks) {
+    if (!lastImport) {
+      console.log(filter);
       return;
     }
+
+    let { stocks } = lastImport;
 
     if (n) {
       stocks = stocks.slice(0, n);
     }
-    showStocksTable(stocks);
+    console.log(stocks);
+    // showStocksTable(stocks);
 
     process.exit();
   });
-
-program
-  .command("find [codes]")
-  .description(
-    "Get stocks with default ranking options. Stocks between quotes separatted by commas"
-  )
-  .action(async (rawCodes) => {
-    const codes = rawCodes.replace(/\s*/g, "").split(",");
-    const stocks = await getStocksWithDefaultRankingOptions();
-
-    if (!stocks) {
-      return;
-    }
-
-    let stocksFound = codes.map((code: any) => {
-      return stocks.find(
-        (stock) => stock.code.toLowerCase() === code.toLowerCase()
-      );
-    });
-
-    stocksFound = stocksFound.filter((stock: any) => stock);
-
-    if (stocksFound) {
-      showStocksTable(stocksFound);
-    } else {
-      console.log("Stocks not found");
-    }
-
-    process.exit();
-  });
-
 program.parse(process.argv);
--- src/resolvers/stock-import.ts
+++ src/resolvers/stock-import.ts
@@ -1,8 +1,6 @@
 import { Query, Resolver } from "type-graphql";
 
 import getLastImport from "../services/get-last-import";
-import getStocksWithDefaultRankingOptions from "../services/get-stocks-with-default-ranking-options";
-import StockWithRankingAndScore from "../types/stock-with-ranking-and-score";
 import StocksImport from "../types/stocks-import";
 
 @Resolver()
@@ -10,12 +8,6 @@ export class StocksImportResolver {
   @Query(() => StocksImport)
   async currentStocks() {
     const stocks = await getLastImport();
-    return stocks;
-  }
-
-  @Query(() => [StockWithRankingAndScore])
-  async stocksByRecommendedRanking() {
-    const stocks = await getStocksWithDefaultRankingOptions();
     return stocks;
   }
 }
