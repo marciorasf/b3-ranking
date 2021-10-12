8f79a998bedae3a8166adffd5dc758a093ea9ae9
Marcio Souza Filho <marciorasf@gmail.com>
2021-10-12 18:08:27 -0300

    Rename stocks files.

--- src/domain/functions/get-stocks-codes.ts
+++ src/domain/functions/get-stocks-codes.ts
@@ -1,7 +1,8 @@
+import { IS_DEV } from "@config/env";
 import fs from "fs";
 
 export default async function getStocksCodes(): Promise<string[]> {
-  const file = "resources/stocks_codes_custom.json";
+  const file = IS_DEV ? "resources/stock_codes_dev.json" : "resources/stock_codes_custom.json";
 
   const rawFile = (await fs.promises.readFile(file)) as unknown as string;
   const availableStocks = JSON.parse(rawFile).stocks;
