f12b5c7169bebbff137251532f0da5ea90c3e222
Marcio Souza Filho <marciorasf@gmail.com>
2021-12-15 21:30:54 -0300

    Handle not connecting to mongo.

--- src/mongo.ts
+++ src/mongo.ts
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 import { connectionOptions, MONGO_URI } from "@config/mongo";
 
-mongoose.connect(MONGO_URI, connectionOptions);
+mongoose
+  .connect(MONGO_URI, connectionOptions)
+  .catch((err: any) => console.error(`Error while connecting with MongoDB: ${err}`));
+
+mongoose.set("bufferCommands", false);
 
 export default mongoose;
